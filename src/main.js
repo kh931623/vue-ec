33424a82041d3fac3ea591f5be22d113b0581144
kh931623 <kh931623@hotmail.com>
2019-05-05 01:28:56 +0800

    add vue-devtool

--- src/main.js
+++ src/main.js
@@ -7,11 +7,14 @@ import 'vuetify/dist/vuetify.min.css'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.config.devtools = true;
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
 Vue.use(Vuetify);
 
-new Vue({
+const app = new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor
